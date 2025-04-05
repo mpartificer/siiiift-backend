a8ab5bc941210e2081ef4b6abb74c3f231ec2804
mpartificer <megan.forsey@hotmail.com>
2025-04-04 22:37:42 -0230

    Local server up and running!

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'werk' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
